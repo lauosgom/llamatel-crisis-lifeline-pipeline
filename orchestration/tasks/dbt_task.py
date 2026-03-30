144b868dbde0144d44518b48939b8348708e4aea
lauosgom <lauosgom@gmail.com>
2026-03-30 00:14:51 +0000

    fixing dbt3

--- orchestration/tasks/dbt_task.py
+++ orchestration/tasks/dbt_task.py
@@ -19,6 +19,17 @@ def dbt_task() -> None:
     env["BQ_PRIVATE_KEY"] = gcs_creds.get("private_key", "")
     env["BQ_CLIENT_EMAIL"] = gcs_creds.get("client_email", "")
     env["BQ_CLIENT_ID"] = gcs_creds.get("client_id", "")
+
+    # run seeds first
+    result_seed = subprocess.run(
+        [str(DBT_BIN), "seed"],
+        cwd=str(DBT_PROJECT_DIR),
+        capture_output=True,
+        text=True,
+        env=env
+    )
+    print("SEED STDOUT:", result_seed.stdout)
+    print("SEED STDERR:", result_seed.stderr)
     
     result = subprocess.run(
         [str(DBT_BIN), "run"],
